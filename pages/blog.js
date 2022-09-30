4fcbb12ae6c29d575332c9e29a3f33f6fdf7afc3
Aayush2492 <aayushpatel2402@gmail.com>
2022-09-30 13:48:13 +0530

    Move blogs from home to a separate blog page.

---
+++ pages/blog.js
@@ -0,0 +1,44 @@
+import Layout from "../components/layout";
+import Head from "next/head";
+import Container from "../components/container";
+import HomeRedirect from "../components/home-redirect";
+import HorizontalRule from "../components/HorizontalRule";
+import Trio from "../components/trio";
+import { getAllPosts } from "../lib/api";
+
+export default function Blog({ allPosts }) {
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>&lt; Lambda /&gt; | Blog</title>
+        </Head>
+        <Container>
+          <HomeRedirect />
+          <h1 className="text-3xl sm:text-4xl md:text-6xl dark:text-gray-100 font-extrabold leading-9 sm:leading-10 md:leading-14 tracking-tight pb-2">
+            Blog
+          </h1>
+          <HorizontalRule />
+          <Trio heroPost={heroPost} morePosts={morePosts} />
+        </Container>
+      </Layout>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const allPosts = getAllPosts([
+    "title",
+    "date",
+    "slug",
+    "author",
+    "coverImage",
+    "excerpt",
+  ]);
+
+  return {
+    props: { allPosts },
+  };
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-import Container from '../components/container'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-import Trio from '../components/trio'
+import Container from "../components/container";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
+import Head from "next/head";
 
-export default function Index({ allPosts }) {
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
+export default function Index() {
   return (
     <>
       <Layout>
@@ -16,25 +12,9 @@ export default function Index({ allPosts }) {
         </Head>
         <Container>
           <Intro />
-          <Trio heroPost={heroPost} morePosts={morePosts}/>
           {/* Will loop over this Trio element to render more posts */}
         </Container>
       </Layout>
     </>
-  )
-}
-
-export async function getStaticProps() {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-  ])
-
-  return {
-    props: { allPosts },
-  }
+  );
 }
