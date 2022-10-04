1e435601eab0828ebc8e737539cdece1a3eb0429
Aayush2492 <aayushpatel2402@gmail.com>
2022-10-05 02:25:32 +0530

    Final touchups

--- pages/about.js
+++ pages/about.js
@@ -18,19 +18,9 @@ export default function About() {
           </h1>
           <HorizontalRule />
           <p className="pt-5 text-sm sm:text-base md:text-lg dark:text-gray-100 sm:leading-10 md:leading-14 tracking-tight">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-            elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin
-            eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet.
-            Duis dapibus diam vel metus tempus vulputate.Sed ut perspiciatis
-            unde omnis iste natus error sit voluptatem accusantium doloremque
-            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-            veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
-            fugit, sed quia consequuntur magni dolores eos qui ratione
-            voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
-            ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non
-            numquam eius modi tempora incidunt ut labore et dolore magnam
-            aliquam quaerat voluptatem.
+            We are the software development club of IITH. We build solutions and
+            help others to do the same. We are a community of developers. Over
+            the years, we have built application useful for the IITH-community.
           </p>
         </Container>
       </Layout>
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,12 @@ import Container from "../components/container";
 import Intro from "../components/intro";
 import Layout from "../components/layout";
 import Head from "next/head";
+import Trio from "../components/trio";
+import { getAllPosts } from "../lib/api";
 
-export default function Index() {
+export default function Index({ allPosts }) {
+  const heroPost = allPosts[0];
+
   return (
     <>
       <Layout>
@@ -13,8 +17,26 @@ export default function Index() {
         <Container>
           <Intro />
           {/* Will loop over this Trio element to render more posts */}
+          <Container>
+            <Trio heroPost={heroPost} morePosts={[]} />
+          </Container>
         </Container>
       </Layout>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const allPosts = getAllPosts([
+    "title",
+    "date",
+    "slug",
+    "author",
+    "coverImage",
+    "excerpt",
+  ]);
+
+  return {
+    props: { allPosts },
+  };
 }
