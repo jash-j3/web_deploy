8e2050d7d11a775fadb309ae535626be04aeeac0
Aayush2492 <aayushpatel2402@gmail.com>
2022-10-12 19:10:56 +0530

    Fix light mode bug

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Script from "next/script";
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en" class="dark">
         <Head />
         <body className="bg-slate-800">
           <Main />
