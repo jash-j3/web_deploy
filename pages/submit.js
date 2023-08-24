2ac31b50794c50e68d929d97b8fc4e75d0f7b24d
jash-j3 <jash.jhatakia@gmail.com>
2023-08-24 15:53:37 +0530

    Finished the get feature

--- pages/submit.js
+++ pages/submit.js
@@ -1,88 +1,3 @@
-// import { useState } from "react";
-// import { useSession } from "next-auth/react";
-// import Head from "next/head";
-// import Link from "next/link";
-// import TextBox from "../components/textbox";
-// import Layout from "../components/layout";
-// import Container from "../components/container";
-// import { useRouter } from "next/router";
-// const GUESS_COUNT = 10;
-// export default function SubmitGuesses() {
-//   let [guesses, setGuesses] = useState(Array(GUESS_COUNT).fill(""));
-//   const placeholders =
-//     "what do you think we'll talk about in today's orientation".split(" ");
-//   const { data: session } = useSession();
-//   console.log(session);
-//   const router = useRouter();
-//   async function uploadGuesses(guesses, email) {
-//     if (guesses.indexOf("") > -1) {
-//       alert("Please fill out all the guesses");
-//       return;
-//     }
-//     const endpoint = "/api/post";
-//     //   let email = "cs20btech11001@iith.ac.in";
-//     const data = {
-//       _id: email,
-//       guesses: guesses,
-//     };
-//     const jsondata = JSON.stringify(data);
-//     const options = {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: jsondata,
-//     };
-//     // const response = await fetch(endpoint, options);
-//     const response = await fetch(endpoint, options)
-//       .then((res) => {
-//         console.log("res", res);
-//         router.push("/orientation");
-//       })
-//       .catch((err) => {
-//         console.log("err", err);
-//       });
-//   }
-//   return (
-//     <Layout>
-//       <Head>
-//         <title>&lt; Lambda /&gt; | Submit</title>
-//       </Head>
-//       <Container>
-//         <>
-//           <div className="mb-10 content-center">
-//             {guesses.map((guess, i) => {
-//               return (
-//                 <>
-//                   <div className="pt-5 dark:text-gray-50/80">Guess {i + 1}</div>
-//                   <TextBox
-//                     placeholder={placeholders[i]}
-//                     value={guess}
-//                     onChange={(e) => {
-//                       const copiedArray = Array.from(guesses);
-//                       copiedArray[i] = e.target.value;
-//                       setGuesses(copiedArray);
-//                     }}
-//                   />
-//                 </>
-//               );
-//             })}
-//           </div>
-//           <div className="mb-5">
-//             <button
-//               type="button"
-//               className="px-4 py-2 bg-neutral-900 dark:text-gray-50/80 rounded-3xl"
-//               onClick={() => uploadGuesses(guesses, session.user.email)}
-//             >
-//               Submit
-//             </button>
-//           </div>
-//         </>
-//       </Container>
-//     </Layout>
-//   );
-// }
-
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
@@ -96,22 +11,25 @@ const QUESTION_COUNT = 7;
 export default function SubmitGuesses() {
   const [questions, setQuestions] = useState(Array(QUESTION_COUNT).fill(""));
   const [answered, setAnswered] = useState(Array(QUESTION_COUNT).fill(false));
+  const [data1, setData1] = useState([]);
   const { data: session } = useSession();
+  const [email, setEmail] = useState("");
   const router = useRouter();
 
-  const options1 = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ email: "cs22btech11028@iith.ac.in" }),
-  };
+
   async function getanswers() {
+    const options1 = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: "cs22btech11028@iith.ac.in" }),
+    };
     try {
       const response = await fetch(getpoint, options1);
       if (response.ok) {
-        let data = await response.json();
-        return data;
+        let data1 = await response.json();
+        setData1(data1);
       } else {
         console.error("Failed to get answer");
       }
@@ -119,18 +37,35 @@ export default function SubmitGuesses() {
       console.log("jg", error);
     }
   }
+  function MyComponent() {
+    useEffect(() => {
+      getanswers();
+    }, []);}
 
-  const fetchData = async () => {
-    const response = await getanswers();
+  
+  // const fetchData = async () => {
+  //   const response = await getanswers();
+  //   let answ;
+  //   for (answ in response) {
+  //     console.log(response)
+  //     console.log(response[answ].questionIndex);
+  //     answered[response[answ].questionIndex] = true;
+  //   }
+  // };
+
+  function workData(response){
+    MyComponent()
     let answ;
     for (answ in response) {
       console.log(response)
       console.log(response[answ].questionIndex);
       answered[response[answ].questionIndex] = true;
     }
-  };
+    
+  }
+  workData(data1);
 
-  fetchData();
+  
   async function uploadAnswer(questionIndex, answer) {
     if (!session) {
       router.push("/orientation");
@@ -194,8 +129,8 @@ export default function SubmitGuesses() {
       <Container>
         <>
           <div className="mb-10 content-center">
+            
             {questions.map((question, i) => {
-              fetchData();
               return (
                 <div key={i} className="mb-5">
                   <div className="pt-5 dark:text-gray-50/80">
